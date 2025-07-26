28fecbb0c036c38c96c7ac8f91ed5ec8433f610c
Saurav Raj <er.sauravraj12@gmail.com>
2025-07-27 01:58:51 +0530

    checking 1

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { Toaster } from "react-hot-toast";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter basename="/StudyNotion-App">
+  <BrowserRouter basename="/UpSkillr">
     <App />
     <Toaster />
   </BrowserRouter>
